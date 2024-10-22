5fc17a49a77f8bcef13e1dcb313fce9dad4e45ea
Alex Ilushyk <ailushyk@gmail.com>
2024-10-22 08:48:36 +0200

    chore: cleanup sleep

--- src/libs/api/fetch-with-params.ts
+++ src/libs/api/fetch-with-params.ts
@@ -1,14 +1,11 @@
 import { env } from '@/env.ts'
 import { fetcher } from '@/libs/api/fetcher.ts'
-import { sleep } from '@/libs/sleep.ts'
 import { ApiRequestParams } from '@/types.ts'
 
 export const fetchWithParams = async <T, P extends ApiRequestParams>(
   url: string,
   { page, limit, sort, order, ...params }: P,
 ) => {
-  await sleep(300) // TODO: delay for demo purposes
-
   const queryParams = {
     ...((page || page === '1') && { _page: page }),
     ...((limit || limit === String(env.LIMIT)) && { _limit: limit }),
